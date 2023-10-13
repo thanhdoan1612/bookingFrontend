df2be188864a7da8ca2ade5fb3c3a74b2d4f1051
LENOVO <20230407@st.hcmuaf.edu.vn>
2023-10-13 21:38:53 +0700

    add new file

---
+++ src/App.js
@@ -0,0 +1,27 @@
+
+import React from 'react';
+import './css/plugins.css';
+import './css/skin/skin-1.css';
+import './css/style.css';
+import './css/templete.css';
+import './css/header.css'
+import './plugins/fontawesome/css/font-awesome.min.css';
+import './plugins/slick/slick-theme.min.css';
+import './plugins/slick/slick.min.css';
+
+import Routes from "./route/routes/Routes";
+
+
+function App() {
+
+
+    return (
+
+        <div className="App">
+            <Routes />
+        </div>
+    )
+}
+
+
+export default App;
